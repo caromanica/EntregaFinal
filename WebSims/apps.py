3706a859ab398f49c5c28199db6daa96390339a9
CaroManica <caromanica@hotmail.com>
2023-08-24 18:17:30 -0300

    primer commit

---
+++ WebSims/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebsimsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'WebSims'
---
+++ WebSims/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=50)
+    usuario=models.CharField(max_length=30)
+    email=models.EmailField()
+
+
+class Modder(models.Model):
+    usuario= models.CharField(max_length=30)
+    website= models.URLField()
+    email= models.EmailField()
+
+class Mod(models.Model):
+    nombre=models.CharField(max_length=50)
+    creador= models.CharField(max_length=30)
+    download_link = models.URLField()
