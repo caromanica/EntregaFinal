8577d267aaa4d48345ebd2f443f2ad27959b0277
CaroManica <caromanica@hotmail.com>
2023-08-24 22:57:59 -0300

    views y url para models agregados

--- WebSims/urls.py
+++ WebSims/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
+from django.contrib import admin
 from WebSims import views
-from WebSims.views import saludo
+from WebSims.views import *
 
 
 
 urlpatterns = [
-    path('saludo/', saludo)
+    path('saludo/', saludo),
+    path('registrousuarios/', registrar_usuario),
+    path('registromods/', registrar_mod),
+    path('listademods/', listar_mods),
+    path('listadeusuarios/', listar_usuarios)
+     
 ]
 
--- WebSims/views.py
+++ WebSims/views.py
@@ -2,5 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from WebSims.models import Usuario, Modder, Mod
 
+
 def saludo(request): #probando
     return HttpResponse("probando probando")
+
+
+def registrar_usuario(request):
+    nombre_usuario="Eliza Pancakes"
+    print("Registrando usuario")
+    usuario=Usuario(nombre=nombre_usuario)
+    usuario.save()
+    respuesta=f"Usuario creado: {usuario.nombre}"
+    return HttpResponse(respuesta)
+
+def registrar_mod(request):
+    nombre_mod="Wonderful Whims"
+    creador_mod="TurboDriver"
+    print("Registrando mod en la web")
+    mod=Mod(nombre=nombre_mod, creador=creador_mod)
+    mod.save()
+    respuesta=f"Mod {mod.nombre}, creado por {mod.creador} cargado a la web"
+    return HttpResponse(respuesta)
+
+def listar_mods(request):
+    mods=Mod.objects.all()
+    respuesta=""
+    for mod in mods:
+        respuesta+=f"{mod.nombre} - {mod.creador}<br>" 
+    return HttpResponse(respuesta)    
+    
+def listar_usuarios(request):
+    usuarios=Usuario.objects.all()
+    respuesta=""
+    for usuario in usuarios:
+        respuesta+=f"{usuario.nombre}<br>" 
+    return HttpResponse(respuesta)    
+    
+
+
+
